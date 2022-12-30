2c5040fdd364d05f1339da0ee30d5cbc0ab98688
Jaz Ong <jazkong1@gmail.com>
2022-12-30 12:29:49 -0800

    Fixed up the styling issues from previous PR

--- react/src/features/VerifyFacial.tsx
+++ react/src/features/VerifyFacial.tsx
@@ -1,34 +1,18 @@
 import React, { FormEvent, useEffect, useState } from "react";
 import { convertImgToBase64String } from "../utils";
 import { FacialDetails } from "../Types";
+import "./VerifyFacial.css";
 
 const defaultFaceInfo: FacialDetails = {
-  verified: "",
+  verified: "false",
 };
 
-const VERIFY_POST_URL = "http://127.0.0.1:8000/verify";
+const VERIFY_POST_URL = "http://localhost:8000/verify";
 
 export const FaceFeatures = () => {
-  let imgStyles;
   const [firstImg, setFirstImg] = useState("");
   const [secondImg, setSecondImg] = useState("");
   const [faceCompare, setFaceData] = useState(defaultFaceInfo);
-
-  if (firstImg && secondImg) {
-    if (`${faceCompare.verified}` == "true") {
-      imgStyles = {
-        border: "5px solid rgba(0, 256, 0, 0.75)",
-        width: "500px",
-        height: "500px",
-      };
-    } else {
-      imgStyles = {
-        border: "5px solid rgba(256, 0, 0, 0.75)",
-        width: "500px",
-        height: "500px",
-      };
-    }
-  }
 
   const uploadImgCallback = (
     event: FormEvent<HTMLInputElement> | undefined
@@ -70,10 +54,16 @@ export const FaceFeatures = () => {
       <input onInput={uploadImgCallback} type={"file"} />
       <input onInput={uploadImgCallback2} type={"file"} />
       <div>
-        <img style={imgStyles} src={firstImg}></img>
-        <img style={imgStyles} src={secondImg}></img>
-
-        <p>{`Same person? ${faceCompare.verified}`}</p>
+        {firstImg && secondImg &&<img
+          className={faceCompare.verified ? "verified" : "not-verified"}
+          src={firstImg}
+        ></img>}
+        {firstImg && secondImg &&<img
+          className={faceCompare.verified ? "verified" : "not-verified"} 
+          src={secondImg}
+        ></img>}
+        
+        {firstImg && secondImg &&<p>{`Same person? ${faceCompare.verified}`}</p>}
       </div>
     </>
   );
